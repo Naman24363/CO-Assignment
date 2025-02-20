ee6bb382174cffd5708ec8577868ab9329ab13d5
Naman Chug <naman24363@iiitd.ac.in>
2025-02-20 06:23:27 +0000

    Added Input and Output file reading

--- Assembler.py
+++ Assembler.py
@@ -1,2 +1,15 @@
 #This is our Assembler
 #This is for R,S,I,J,B type instructions
+
+
+import sys
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+f_ans = [] 
+with open(input_file, 'r') as file:
+    lines = file.readlines()
+
+with open(output_file, 'w') as file:
+    for output in f_ans:
+        file.write(output + '\n')
