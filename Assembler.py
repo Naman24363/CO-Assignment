d58a57bc2f1adfd9f91c59b39c88e99ef38cb013
cho2002 <cho24165@iiitd.ac.in>
2025-02-20 12:28:19 +0530

    I-Type Instruction

--- Assembler.py
+++ Assembler.py
@@ -71,6 +71,39 @@ I_Type = {
         "jalr": {"opcode": "1100111", "funct3": "000"}
 }
 
+# I-Type Instruction
+    elif data.split()[0] in I_Type:
+        opcode = I_Type[data.split()[0]]["opcode"]
+        funct3 = I_Type[data.split()[0]]["funct3"]
+        data = data.replace(",", " ")
+
+        if len(data.split()) == 3:
+            input_str = data.split()[2]
+            opening_index = input_str.find('(')
+            number_string = input_str[:opening_index]
+            decimal = int(number_string)
+            opening_index = input_str.find('(')
+            closing_index = input_str.find(')')
+            fin_reg = input_str[opening_index + 1:closing_index]
+        else:
+            decimal = int(data.split()[3])
+            fin_reg = data.split()[2]
+
+        if decimal < 0:
+            bina = bin(abs(decimal))[2:].zfill(12)
+            bina = bina.replace("0", "X").replace("1", "0").replace("X", "1")
+            result = funct1(bina, "1")
+            imm = result
+        else:
+            imm = bin(decimal)[2:].zfill(12)
+
+        if fin_reg not in Registers or data.split()[1] not in Registers:
+            f_ans.append("Error: Invalid register")
+            continue
+
+        output = imm + Registers[fin_reg] + funct3 + Registers[data.split()[1]] + opcode
+        f_ans.append(output)
+
 # S-Type Instruction
     elif data.split()[0] in S_Type:
         opcode = S_Type[data.split()[0]]["opcode"]
@@ -98,3 +131,9 @@ I_Type = {
 
         output = bina[0:7] + Registers[data.split()[1]] + Registers[substring] + funct3 + bina[7:] + opcode
         f_ans.append(output)
+
+
+
+with open(output_file, 'w') as file:
+    for output in f_ans:
+        file.write(output + '\n')
